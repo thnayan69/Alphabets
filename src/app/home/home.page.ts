dfd19fc51056d83fe76f0c7e04f764f386b1e848
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2019-09-18 09:02:28 +0600

    Contents moved to object

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,66 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  Data: any = [
+    {
+      alphabet: 'A',
+      word: 'Apple',
+      image: ''
+    },
+    {
+      alphabet: 'B',
+      word: 'Ball',
+      image: ''
+    },
+    {
+      alphabet: 'C',
+      word: 'Cat',
+      image: ''
+    },
+    {
+      alphabet: 'D',
+      word: 'Doll',
+      image: ''
+    },
+    {
+      alphabet: 'E',
+      word: 'Egg',
+      image: ''
+    },
+    {
+      alphabet: 'F',
+      word: 'Flower',
+      image: ''
+    },
+    {
+      alphabet: 'G',
+      word: 'Giraffe',
+      image: ''
+    },
+    {
+      alphabet: 'H',
+      word: 'Horse',
+      image: ''
+    },
+    {
+      alphabet: 'I',
+      word: 'Ink',
+      image: '',
+    },
+    {
+      alphabet: 'J',
+      word: 'Jeep',
+      image: ''
+    },
+    {
+      alphabet: 'K',
+      word: 'Kite',
+      image: ''
+    },
+    {
+      alphabet: 'L',
+      word: 'Lion',
 
+    }
+  ];
 }
